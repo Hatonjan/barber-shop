948818729e9788ecaea043d7638b6a20535846df
Jonathan <mj.scot.r@gmail.com>
2025-09-23 19:35:20 -0700

    First commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// Initialize the AOS animations
+AOS.init({
+    offset: 120, // triggers 120px before element enters view
+    duration: 1000,
+    once: false, // allow animation every time it scrolls into view
+    mirror: false // disable animation on scroll up
+});
